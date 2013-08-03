03e9b042ea9b25dfe727106575b21f3afbd8f3be
Andrew Macheret <andrew.macheret@gmail.com>
2013-08-03 16:37:25 -0700

    updated setup process

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+npm install express
+npm install file
+npm install forever
+npm install socket.io
+npm install mongodb
+npm install mongoose
+npm install mongoose-types
+npm install passport
+npm install passport-facebook
+npm install passport-google
+npm install passport.socketio
+npm install when
